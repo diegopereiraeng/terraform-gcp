5a4096b09feb61fef4f11ff11d577da4d1754ae2
Diego Paes Ramalho Pereira <koaladiego@Diego-Pereira-MBP.local>
2021-04-13 13:17:56 -0400

    Terraform GCP

---
+++ VM/main.tf
@@ -0,0 +1,49 @@
+
+
+// Configure the Google Cloud provider
+provider "google" {
+ credentials = file(var.gcp_sa) //
+ project     = var.project //
+ region      = var.region //"us-central1-a"
+}
+
+// Terraform plugin for creating random ids
+resource "random_id" "instance_id" {
+ byte_length = 8
+}
+
+// A single Compute Engine instance
+resource "google_compute_instance" "default" {
+ name         = "diego-vm-${random_id.instance_id.hex}"
+ machine_type = var.instance_type // "f1-micro"
+ zone         = var.region // "us-central1-a"
+ ssh-keys = "INSERT_USERNAME:${file(var.ssh_keys)}"  // "~/.ssh/id_rsa.pub")}"
+ 
+ boot_disk {
+   initialize_params {
+     image = "debian-cloud/debian-9"
+   }
+ }
+
+// Make sure nodejs is installed on all new instances for later steps
+ metadata_startup_script = "sudo apt-get update; sudo apt-get install -y nodejs"
+
+ network_interface {
+   network = "default"
+
+   access_config {
+     // Include this section to give the VM an external ip address
+   }
+ }
+}
+
+
+resource "google_compute_firewall" "default" {
+ name    = "diego-app-firewall"
+ network = "default"
+
+ allow {
+   protocol = "tcp"
+   ports    = ["3000"]
+ }
+}
---
+++ VM/variables.tf
@@ -0,0 +1,28 @@
+variable "project" {
+  description = "The Google project to be used"
+  type        = string
+  default     = ""
+}
+
+variable "region" {
+  description = "Resource Region"
+  type        = string
+}
+
+variable "gcp_sa" {
+  description = "The Google Service Account to be used to create the resources"
+  type        = string
+  default     = ""
+}
+
+variable "ssh_keys" {
+  description = "The ssh keys to connect to GCP VM Instance"
+  type        = string
+  default     = ""
+}
+
+variable "instance_type" {
+  description = "The VM Instance Type"
+  type        = string
+  default     = ""
+}
