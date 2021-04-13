7058127efe41f3347c8d464395b3c0b1b61bb714
Diego Paes Ramalho Pereira <koaladiego@Diego-Pereira-MBP.local>
2021-04-13 18:03:22 -0400

    Terraform GCP

--- VM/output.tf
+++ VM/output.tf
@@ -1,4 +1,5 @@
 // A variable for extracting the external IP address of the instance
-output "ip" {
- value = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
+output "public_ip" {
+    description = "List of public IP addresses assigned to the instances, if applicable"
+    value = google_compute_instance.default.network_interface.0.access_config.0.nat_ip
 }
